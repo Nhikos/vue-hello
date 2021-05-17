a77ca3398da573753d63ba8dbd02ce85576e38c6
Nhikos <4ndre.sergi@gmail.com>
2021-05-17 22:38:32 +0200

    Ex complete

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
+
+var app = new Vue(
+    {
+        el: "#root",
+        data: {
+            messaggio: "",
+            colorClass: "red",
+            immagine: "https://i.redd.it/6rznv17uitm21.jpg"
+            }
+    }
+);
